df3b17bc5df937b4cbde1d22c70ad92feb9852e7
none <none>
2023-01-06 11:30:41 +0900

    fix

--- app.py
+++ app.py
@@ -2,34 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-footer = """<style>
-a:link , a:visited{
-color: blue;
-background-color: transparent;
-text-decoration: underline;
-}
-a:hover,  a:active {
-color: red;
-background-color: transparent;
-text-decoration: underline;
-}
-.footer {
-position: fixed;
-left: 0;
-bottom: 0;
-height: 25px;
-width: 100%;
-font_size="0.8rem";
-background-color: grey;
-color: lightgrey;
-text-align: center;
-z-index: 100;
-}
-</style>
-<div class="footer">
-<p>Developed by <a href="https://twitter.com/kawaiiprompter" target="_blank">chili@kawaiiprompter</a></p>
-</div>
-"""
+from footer import footer
 
 def load_csv(filename):
     datas = []
@@ -51,10 +24,13 @@ def load_cache():
     return movements, mediums, flavors, danbooru_tags
 
 
-def search(search_word, word_list):
+def search(search_words, word_list):
     results = []
     for w in word_list:
-        if search_word in w:
+        if all([
+            search_word in w
+            for search_word in search_words.split(" ")
+            ]):
             results.append(w)
     return results
 
@@ -63,9 +39,9 @@ def main():
     st.set_page_config(page_title="Word Searcher")
     st.markdown(footer, unsafe_allow_html=True)
     
-    st.markdown("## search word in wd14-tagger & CLIP Interrogator")
+    st.markdown("## Finding word from wd14-tagger & CLIP Interrogator")
     movements, mediums, flavors, danbooru_tags = load_cache()
-    search_word = st.text_input("検索ワード").lower()
+    search_words = st.text_input("検索ワード").lower()
     max_size = st.selectbox("表示最大数", [20, 100, 200, 500, 1000])
 
     labels = [
@@ -79,10 +55,16 @@ def main():
         movements,
     ]
     
-    if search_word != "":
+    if search_words != "":
         # WD14
-        pick_tags = danbooru_tags[danbooru_tags["name"].map(lambda x: search_word in x)]
-        st.markdown(f"### wd14-tagger")
+        def map_search(target_word):
+            flg_exist_list = [
+                search_word in target_word
+                for search_word in search_words.split(" ")
+            ]
+            return all(flg_exist_list)
+        pick_tags = danbooru_tags[danbooru_tags["name"].map(map_search)]
+        st.markdown("### wd14-tagger")
         if len(pick_tags) == 0:
             st.text("search results: 0")
         else:
@@ -95,7 +77,7 @@ def main():
         # CLIP Interrogator
         for label, word_list in zip(labels, v_lists):
             st.markdown(f"### {label}")
-            pick_word_list = search(search_word, word_list)
+            pick_word_list = search(search_words, word_list)
             if len(pick_word_list) == 0:
                 st.text("search results: 0")
             else:
